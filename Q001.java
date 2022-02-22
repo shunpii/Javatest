d0ab6aa06d8580f2bf9de4ee279c30c1aa3c7bed
shunpii <shun.s0029@gmail.com>
2022-02-22 18:14:37 +0900

    [finish]横並びの繰り返し

--- Q001.java
+++ Q001.java
@@ -1,2 +1,9 @@
 public class Q001 {
+    public static void main(String[] args) {
+        // 横に並べる（繰り返し）
+        // 文字「■」を横に5つ並べて表示させてください。
+        for (int i = 0; i < 5; i++) {
+            System.out.print("■");
+        }
+    }
 }
