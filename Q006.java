5dd215130e91397ba3415ed3b047f65018733940
shunpii <shun.s0029@gmail.com>
2022-02-22 18:42:23 +0900

    [add]2つの文字を交互に並べる（条件分岐と繰り返し）

--- Q006.java
+++ Q006.java
@@ -1,3 +1,17 @@
 public class Q006 {
-    
+    public static void main(String[] args) {
+        // 2つの文字を交互に並べる（条件分岐と繰り返し）
+        // 文字「◇」と文字「◆」を交互に表示させてください。
+        // ただし、並べる文字数はプログラムの先頭で変数に代入することで指定してください
+
+        int n = 11;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                System.out.print("◆");
+            } else {
+                System.out.print("◇");
+            }
+        }
+    }
 }
