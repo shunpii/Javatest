5c1b39cfafba989edc2e87f2dedfdcb8be5554db
shunpii <shun.s0029@gmail.com>
2022-03-01 16:11:14 +0900

    [finish]配列の内容を表示

---
+++ Q024.java
@@ -0,0 +1,14 @@
+public class Q024 {
+    public static void main(String[] args) {
+
+        // 配列の内容を表示
+        // 配列に格納されたデータを先頭から順に表示させてください。
+        // ただし、操作対象の配列はプログラムの先頭で宣言し、適当な初期値を代入してください。
+
+        int[] n = {10, 20, 30, 40, 50};
+
+        for (int i : n) {
+            System.out.println(i);
+        }
+    }
+}
