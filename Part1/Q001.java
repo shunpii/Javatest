53501c3bd201cdb30d981a8987fff34514ed6d09
shunpii <shun.s0029@gmail.com>
2022-03-10 18:22:47 +0900

    [finish]Q001

---
+++ Part1/Q001.java
@@ -0,0 +1,15 @@
+package Part1;
+
+public class Q001 {
+    // 「aが7より大きいならtrue」という式を記述し、
+    // 実行結果を得てください。
+
+    public static void main(String[] args) {
+        int a = 12;
+
+        if (a > 7) {
+            System.out.println("aは７よりおおきいです。");
+        }
+    }
+    
+}
