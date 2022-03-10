ec155e5a3128e2a5610cb0c6162845a7fa893460
shunpii <shun.s0029@gmail.com>
2022-03-10 18:29:34 +0900

    [finish]Q002

---
+++ Part1/Q002.java
@@ -0,0 +1,19 @@
+package Part1;
+
+public class Q002 {
+    // 下記を行ない、実行結果を得てください。
+
+    // 提供コード内の①に「aが8以上ならtrue」という式を記述します。
+    // 提供コード内の7行目にelseブロックを追加します。ブロック内には
+    // 「aは8より小さいです」と表示するプログラムを記述します。
+
+    public static void main(String[] args) {
+        int a = 7;
+
+        if (a > 8) {
+            System.out.println("aは８以上です。");
+        } else {
+            System.out.println("aは8以下です。");
+        }
+    }
+}
